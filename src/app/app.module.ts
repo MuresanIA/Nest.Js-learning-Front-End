49412f861414c7005360406363a11cbb6f0c1d84
Ionut Alexandru Muresan <alexandru.muresan@fortech.ro>
2023-01-11 13:11:20 +0200

    updating angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemsModule } from './items/modules/items.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ConfirmationModalComponent } from './confirmation-modal/confirmation-modal/confirmation-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
+    ConfirmationModalComponent,
   ],
   imports: [
     BrowserModule,
